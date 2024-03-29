26d0482d744aa310195eaf3c615d1761ce5d62fd
Jack Zhang <jackjz1112@gmail.com>
2024-03-29 00:21:08 -0500

    lwr and elasticnet models

--- models/merge_datasets.py
+++ models/merge_datasets.py
@@ -73,21 +73,21 @@ class MergeDatasets(object):
         merged_df = pd.merge(merged_df, land_continent_aggregated, left_on=['Entity', 'Year'], right_on=['Custom Continent', 'Year'], how='inner').drop(columns=['Custom Continent'])
 
         # merge invasive alien species
-        self.invas_df['Custom Continent'] = self.invas_df['Entity'].apply(MergeDatasets.map_country_to_continent)
-        expanded_rows = []
-        for _, row in self.invas_df.iterrows():
-            categories = row['Custom Continent']
-            if categories:
-                for category in categories:
-                    expanded_row = row.to_dict()
-                    expanded_row['Custom Continent'] = category
-                    expanded_rows.append(expanded_row)
+        # self.invas_df['Custom Continent'] = self.invas_df['Entity'].apply(MergeDatasets.map_country_to_continent)
+        # expanded_rows = []
+        # for _, row in self.invas_df.iterrows():
+        #     categories = row['Custom Continent']
+        #     if categories:
+        #         for category in categories:
+        #             expanded_row = row.to_dict()
+        #             expanded_row['Custom Continent'] = category
+        #             expanded_rows.append(expanded_row)
 
-        expanded_invas_df = pd.DataFrame(expanded_rows)
-        invas_continent_aggregated = expanded_invas_df.groupby(['Custom Continent', 'Year'])['15.8.1 - Countries with an allocation from the national budget to manage the threat of invasive alien species (1 = YES, 0 = NO) - ER_IAS_NATBUD'].mean().reset_index()
-        merged_df = pd.merge(merged_df, invas_continent_aggregated, left_on=['Entity', 'Year'], right_on=['Custom Continent', 'Year'], how='inner')
-        merged_df = merged_df.drop(columns=['Custom Continent'])
-        merged_df = merged_df.rename(columns={'15.8.1 - Countries with an allocation from the national budget to manage the threat of invasive alien species (1 = YES, 0 = NO) - ER_IAS_NATBUD': 'Has Budget against invasive species'})
+        # expanded_invas_df = pd.DataFrame(expanded_rows)
+        # invas_continent_aggregated = expanded_invas_df.groupby(['Custom Continent', 'Year'])['15.8.1 - Countries with an allocation from the national budget to manage the threat of invasive alien species (1 = YES, 0 = NO) - ER_IAS_NATBUD'].mean().reset_index()
+        # merged_df = pd.merge(merged_df, invas_continent_aggregated, left_on=['Entity', 'Year'], right_on=['Custom Continent', 'Year'], how='inner')
+        # merged_df = merged_df.drop(columns=['Custom Continent'])
+        # merged_df = merged_df.rename(columns={'15.8.1 - Countries with an allocation from the national budget to manage the threat of invasive alien species (1 = YES, 0 = NO) - ER_IAS_NATBUD': 'Has Budget against invasive species'})
 
         # merge temperature anomaly
         self.temp_df['Custom Continent'] = self.temp_df['Entity'].apply(MergeDatasets.map_country_to_continent)
